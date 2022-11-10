1b3799e88ab1bbacf81bc63392dda9c587a03f4a
ehern378 <enrique.e.hernandez.h@gmail.com>
2022-11-10 13:43:59 -0600

    tank

--- web-and-mobile/tank-domination/main.py
+++ web-and-mobile/tank-domination/main.py
@@ -11,9 +11,31 @@ pygame.display.set_caption("Tank Domination")
 
 system_clock = pygame.time.Clock()
 
+game_tank_svg = pygame.image.load("tank.svg")
+
+game_tank_sprite = pygame.transform.scale(game_tank_svg,(75,75))
+
 game_characteristics = {
     "sky":{
-        "color":{135, 206, 235}
+        "color":(135, 206, 235)
+    },
+    "grass": {
+        "color":(0,255,0),
+            "position": {
+                "y": 0.8 * monitor_display[1]
+            }
+    },
+    "player":{
+        "position": {
+            "x":0.2 * monitor_display[0]
+        },
+        "hp": 1
+    },
+        "cpu":{
+        "position": {
+            "x":0.8 * monitor_display[0] - game_tank_sprite.get_width()
+        },
+        "hp": 1
     }
 }
 
@@ -30,9 +52,38 @@ while game_running_flag:
 
         break
 
-    # Running game mechanics.
-    game_display.fill(game_characteristics{"sky"}{"color"})
+    # Movement
+    key_pressed = pygame.key.get_pressed()
 
+    position_delta = 0
+
+    if key_pressed[pygame.K_LEFT]:
+        position_delta = -1
+    elif key_pressed[pygame.K_RIGHT]:
+        position_delta = 1
+
+    game_characteristics["player"]["position"]["x"] += position_delta
+
+    # Running game mechanics.
+    game_display.fill(game_characteristics["sky"]["color"])
+
+    # Create grass
+    pygame.draw.rect(game_display,game_characteristics["grass"]["color"],
+    pygame.Rect(0, game_characteristics["grass"]["position"]["y"],
+    monitor_display[0], monitor_display[1] - game_characteristics["grass"]["position"]["y"]))
+
+    # Create player and computer
+    game_tank_sprite_player = game_tank_sprite
+
+    game_display.blit(game_tank_sprite_player, {game_characteristics["player"]["position"]["x"], 
+    game_characteristics["grass"]["position"]["y"] - game_tank_sprite_player.get_height()})
+
+    game_tank_sprite_cpu = pygame.transform.flip(game_tank_sprite, True, False)
+
+    game_display.blit(game_tank_sprite_cpu, {game_characteristics["cpu"]["position"]["x"], 
+    game_characteristics["grass"]["position"]["y"] - game_tank_sprite_cpu.get_height()})
+
+    # running game mechanics
     pygame.display.update()
 
     system_clock.tick(30)
