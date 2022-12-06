4c3b7a19e8d5433e0dfca501ce18f86039edb976
ehern378 <enrique.e.hernandez.h@gmail.com>
2022-12-06 13:12:34 -0600

    fix

--- game-analysis/top-10-platforms.py
+++ game-analysis/top-10-platforms.py
@@ -6,11 +6,11 @@ df = pd.read_csv("game-ratings-by-top-10-platforms.csv")
 # group by metrix
 df_follow = df.groupby(["platform_name"])["follow_count"].sum().reset_index()
 
-df_follow = df_follow.rename(colums = {"follow_count" : "total_follows"})
+df_follow = df_follow.rename(columns = {"follow_count" : "total_follows"})
 
 df_hype = df.groupby(["platform_name"])["hype_count"].sum().reset_index()
 
-df_hype = df_hype.rename(colums = {"hype_count" : "total_hypes"})
+df_hype = df_hype.rename(columns = {"hype_count" : "total_hypes"})
 
 #analyza date through vizualization
 BAR_WIDTH = 0.4
